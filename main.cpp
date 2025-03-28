7a08e144953761e02094e10eef020ac813584bb9
X_Nefertar <xnefertar@gmail.com>
2025-03-28 12:18:05 +0800

    Update_Main_Test

--- main.cpp
+++ main.cpp
@@ -8,11 +8,7 @@ int main(){
     // Hello C++ World {} {}
     // Hello C++ WorldHello World
     logger.log("Hello {} {}", "C++", "World");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
     logger.log("Hello {} {} {} {}", "C++", "World");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
     logger.log("Hello {} {} ", "C++", "World", "Hello ", "World");
     std::this_thread::sleep_for(std::chrono::seconds(1));
     return 0;
